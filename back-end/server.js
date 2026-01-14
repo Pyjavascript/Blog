dfb37a9d65ec3de6c1368e0ed50ce747823a5ed6
Coderado <102342706+Pyjavascript@users.noreply.github.com>
2026-01-14 15:16:37 +0000

    started backend code

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,34 @@
 import express from 'express'
 const app = express();
+const articleInfo = [
+    {articleName:"learn-node", upvote:0},
+    {articleName:"learn-react", upvote:0},
+    {articleName:"learn-mongoose", upvote:0},
+]
 
+app.use(express.json())
 app.get('/',(req,res) => {
     res.send('backend is live')
+})
+app.get('/username/:name',(req,res) => {
+    const {name} = req.params;
+    res.send(`your name is ${name}`);
+    
+})
+app.post('/api/articles/:articleName/upvote', (req,res) => {
+    const {articleName} = req.params;
+    const article = articleInfo.find(ar => ar.articleName == articleName)
+    if(!article){
+        res.send('No articlr found')
+    }
+    article.upvote += 1;
+    res.send({
+        messsage:"Success",
+        articleInfo
+    })
+})
+app.get('/data',(req,res) => {
+    res.send(articleInfo)
 })
 
 app.listen(8000, () => {
